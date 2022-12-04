42c839e7a6fdd0cfeda87f0cd11be829454e6182
searinox0902 <svquintana98@gmail.com>
2022-12-04 16:17:03 -0500

    Day 1 challenge complete! :D

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,58 @@
+/* 
+    Create datatypes.js file and use the JavaScript typeof operator 
+    to check different data types. Check the data type of each variable
+*/
+
+console.log(`Type = ${typeof stringVar}`);
+console.log(`Type = ${typeof numberVar}`);
+console.log(`Type = ${typeof arrayVar}`);
+console.log(`Type = ${typeof unknowVar}`);
+console.log(`Type = ${typeof thingVar}`);
+
+
+
+/* 
+    Declare four variables without assigning values
+*/
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+/* 
+    Declare four variables with assigned values
+*/
+
+let var1Assigned = 'string';
+let var2Assigned = 10;
+let var3Assigned = false;
+let var4Assigned = 'string';
+
+
+/* 
+    Declare variables to store your first name, last name, marital status, country and age in multiple lines
+*/
+
+let myName = "Sebastian", lastName = "Vargas", maritalStatus = 0, myCountry = "Colombia", myAge = 24;
+
+
+/* 
+    Declare variables to store your first name, last name, marital status, country and age in a single line
+*/
+
+let myName2 = "Sebastian";
+let myLastName2 = "Vargas";
+let maritalStatus2 = 0;
+let myCountry2 = "Colombia";
+let myAge2 = 24;
+
+
+/*
+    Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+*/
+
+let myAge3 = 24, yourAge = 28;
+
+console.log( `${myAge} & ${yourAge}`);
+
--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,8 @@
-// the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+// Comentario en linea
+// Bienvenido a 30daysOfJavaScript
+
+
+/* 
+    Multiline commentary
+*/
+
